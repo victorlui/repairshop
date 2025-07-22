660cff4823965d898d4f9bccaad081b7ae8cf1e0
Victor Luiz <victorluizgonzalez@gmail.com>
2025-07-22 15:10:24 -0300

    React Table with Next.js, ShadCN, TanStack

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from "next";
 import React from "react";
 import CustomerSearch from "./CustomerSearch";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
+import * as Sentry from "@sentry/nextjs";
+import CustomerTable from "./CustomerTable";
 
 export const metadata: Metadata = {
   title: "Customers",
@@ -16,13 +18,21 @@ const Customers = async ({
 
   if (!searchText) return <CustomerSearch />;
 
+  const span = Sentry.startInactiveSpan({
+    name: "getCustomerSearchResults-2",
+  });
   const results = await getCustomerSearchResults(searchText);
+  span.end();
 
   return (
-    <div>
+    <>
       <CustomerSearch />
-      <p>{JSON.stringify(results)}</p>
-    </div>
+      {results.length ? (
+        <CustomerTable data={results} />
+      ) : (
+        <p className="mt-4">No results found</p>
+      )}
+    </>
   );
 };
 
